9e6b44ed2441d95e558f003953f3f8186a983da0
Anuj Datar <anuj.datar@gmail.com>
2023-07-31 18:24:17 +0530

    Converted tabbed indents to spaces, was inconsistent

--- scripts/ddns-update.sh
+++ scripts/ddns-update.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
 . /config.sh
+
 # #####################################################################
 # define the basic api request
 api_request() {
-	if [ "$METHOD" == "GLOBAL" ]; then
-		curl -sSL \
-		-H "Content-Type: application/json" \
-		-H "X-Auth-Email: $EMAIL" \
-		-H "X-Auth-Key: $API_KEY" \
-		"$@"
-	else
-		curl -sSL \
-		-H "Content-Type: application/json" \
-		-H "Authorization: Bearer $API_KEY" \
-		"$@"
-	fi
+  if [ "$METHOD" == "GLOBAL" ]; then
+    curl -sSL \
+    -H "Content-Type: application/json" \
+    -H "X-Auth-Email: $EMAIL" \
+    -H "X-Auth-Key: $API_KEY" \
+    "$@"
+  else
+    curl -sSL \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $API_KEY" \
+    "$@"
+  fi
 }
+# #####################################################################
 # functions to get public ip
 get_ip4() {
   CURRENT_IP=$(curl -s https://ipv4.icanhazip.com/ || curl -s https://api.ipify.org)
@@ -46,14 +48,14 @@ get_ip6() {
 # #####################################################################
 # Step 1: Get current public IP
 if [ "$RECORD_TYPE" == "A" ]; then
-	CURRENT_IP=$(get_ip4)
+  CURRENT_IP=$(get_ip4)
 elif [ "$RECORD_TYPE" == "AAAA" ]; then
-	CURRENT_IP=$(get_ip6)
+  CURRENT_IP=$(get_ip6)
 fi
 
 if [ -z $CURRENT_IP ]; then
-	echo "[$(date)]: Public IP not found, check internet connection"
-	exit 1
+  echo "[$(date)]: Public IP not found, check internet connection"
+  exit 1
 fi
 # #####################################################################
 # Step 2: Check against old IP
